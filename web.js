8ddceb6f572b7de95cc4862e4a340a189034a518
terrence.tang810 <terrence.tang810@gmail.com>
2012-09-29 02:15:23 +0800

    init

---
+++ web.js
@@ -0,0 +1,29 @@
+var express  = require('express'),
+    app = express.createServer(express.logger());
+
+app.use(express.static(__dirname + "/assert"));
+
+app.get('/admin/parse', function(request, response) {
+      require("./parse.js").run();
+      response.send('Hello World!');
+});
+
+
+
+app.get('/', function(request, response) {
+//      response.send('Hello World!');
+      response.sendfile(__dirname+'/pages/index.html');
+});
+
+app.get('/index', function(request, response) {
+      response.sendfile(__dirname+'/pages/index.html');
+});
+
+app.get('/channel.html', function(request, response) {
+      response.sendfile(__dirname+'/pages/channel.html');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+      console.log("Listening on " + port);
+});
