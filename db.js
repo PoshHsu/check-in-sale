b482027ae7405dd8d97d5a36e6ff47962f9cb4b0
terrence.tang810 <terrence.tang810@gmail.com>
2012-10-01 02:17:15 +0800

    init

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ var DB_URL = "mongodb://heroku_app7984633:ljtiqmuc5so4cvc057vf98vvcq@ds039007.mo
 mongoose.connect(DB_URL);
 
 exports.insertQRCode = function(qrcode){
+    console.log(qrcode);
      mongoose.connection.db.collection(QRCODE_TABLE, function(err, collection) {
        console.log(err);
        collection.insert(qrcode);
