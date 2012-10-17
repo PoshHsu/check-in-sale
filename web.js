0477c30f7bfba4ab19e772f8b6f15aad1f85ec87
squallfan <squall.f@gmail.com>
2012-10-18 02:41:33 +0800

    add upload

--- web.js
+++ web.js
@@ -22,6 +22,25 @@ app.get('/upload', function(req, res) {
 	+'</form></body></html>');
 	res.end();
 });
+
+var fs = require('fs');
+app.post('/fileUpload', function(req, res) {
+
+    var uploadedFile = req.files.uploadingFile;
+        var tmpPath = uploadedFile.path;
+        var targetPath = './' + uploadedFile.name;
+
+        fs.rename(tmpPath, targetPath, function(err) {
+            if (err) throw err;
+               fs.unlink(tmpPath, function() {
+                   
+                   console.log('File Uploaded to ' + targetPath + ' - ' + uploadedFile.size + ' bytes');
+            });
+        });
+    res.send('file upload is done.');
+    res.end();
+});
+
 /* add by squall end */
 
 
