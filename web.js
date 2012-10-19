fca4f48379f59f85234ab0a479138142aafc598b
terrence.tang810 <terrence.tang810@gmail.com>
2012-10-20 03:58:49 +0800

    test

--- web.js
+++ web.js
@@ -70,6 +70,7 @@ app.get('/db/insert/qrcode', function(request, response) {
 
 
 app.get('/website/yahoo', function(request, response) {
+      response.sendfile(__dirname+'/asset/admin_qrcode.html');
       response.sendfile(__dirname+'/asset/redirect.html');
 });
 
